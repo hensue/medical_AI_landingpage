9e7d2321a8892aba621499dd1b740556b2c340d3
Lim Kim San <gentleman97626@gmail.com>
2023-12-17 12:51:19 -0800

    third

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -142,7 +142,7 @@ const HomeHero: FC = () => {
                   }
                 </Typography>
               </Box>
-              <Box sx={{ '& button': { mr: 2 } }}>
+              {/* <Box sx={{ '& button': { mr: 2 } }}>
                 <ScrollLink to="popular-course" spy={true} smooth={true} offset={0} duration={350}>
                   <>
                   <StyledButton color="primary" size="large" variant="contained">
@@ -157,7 +157,7 @@ const HomeHero: FC = () => {
                   </StyledButton>
                   </>
                 </ScrollLink>
-              </Box>
+              </Box> */}
             </Box>
           </Grid>
           <Grid item xs={12} md={5} sx={{ position: 'relative' }}>
